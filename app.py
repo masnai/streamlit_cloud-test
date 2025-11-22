4af5a5d922f1d8477b33f4523663dcb04b33ce49
masnai <mas.naito@gmail.com>
2025-11-22 15:18:53 +0000

    Hello World を作成しました。

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+
+import streamlit as st
+
+# アプリケーションのタイトルを表示
+st.title('🚀 Hello Streamlit World!')
+
+# シンプルなテキストを表示
+st.write('これは、私がStreamlitを使って作った初めてのWebアプリケーションです。')
+
+# ボタンを追加し、クリックされたときにメッセージを表示
+if st.button('メッセージを表示'):
+    st.write('ボタンが押されました！開発頑張りましょう！')
